16d056dfea1e44ee34de1d0e5fe3cbf675b26aac
Amir Tocker <amir.tocker@cloudinary.com>
2019-05-26 17:34:05 +0300

    Refactor message formatting

--- index.js
+++ index.js
@@ -25,16 +25,28 @@ function formatInstructions(source) {
     }, {text:[], style:[]});
     return [all.text.join("\n"), ...all.style];
   } else {
-    return source;
+    return [source];
+  }
+}
+function instructions( {content}){
+  console.log.call(this, ...formatInstructions(content));
+}
+
+function code({code}){
+  if(Array.isArray(code)) {
+    console.log(code.join('\n'));
+  }else {
+
+    console.log(code);
   }
 }
 export function next() {
   let step = nextStep.next().value;
   // console.log(`%c${formatOutput(step.content)}`, "font-family: serif; font-size: 1rem");
-  console.log.call(this, ...formatInstructions(step.content));
-  console.log(step.code.join('\n'));
+  instructions(step);
+  code(step);
   // copy(formatOutput(step.code))
 }
 export function help(){
-  console.log.call(this, ...formatInstructions(messages.messages.help.content));
+  instructions(messages.messages.help);
 }
